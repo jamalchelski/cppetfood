bf2de8cae06eac5cdde936f9ce8091115e11235f
jamalchelski <ifista201522035@gmail.com>
2020-09-20 17:17:24 +0700

    add liblary

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 
 const Splash = ({ navigation }) => {
